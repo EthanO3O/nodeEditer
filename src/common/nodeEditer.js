f65fdef7463f603240429d989b37a3a6aa381855
Ethan <zy_dale@126.com>
2020-01-29 20:18:21 +0800

    update recharge

--- src/common/nodeEditer.js
+++ src/common/nodeEditer.js
@@ -1,4 +1,28 @@
-class NodeEditer {
+import TabBar from "./ui_modules/tabBar"
+import ToolsBar from "./ui_modules/toolsBar"
+import ModulesBar from "./ui_modules/modulesBar"
+import WorkSpace from "./ui_modules/workSpace"
+import MainMenu from "./ui_modules/mainMenu"
+import Revoker from "./func_modules/revoker";
+import SrcGenerator from "./func_modules/srcGenerator";
+import NodeSelector from "./func_modules/nodeSelector";
+import Input from "./func_modules/input";
+import CodeGenerator from "./func_modules/codeGenerator";
 
+class NodeEditer {
+    constructor(){
+        this.tabBar= new TabBar();
+        this.toolsBar= new ToolsBar();
+        this.modulesBar= new ModulesBar();
+        this.workSpace= new WorkSpace();
+        this.mainMenu= new MainMenu();
+        this.revoker= new Revoker();
+        this.srcGenerator= new SrcGenerator();
+        this.nodeSelector= new NodeSelector();
+        this.input = new Input();
+        this.codeGenerator = new CodeGenerator();
+        this.htmlNodeList=[];
+        this.cssNodeList=[];
+    }
 }
 export default NodeEditer
---
+++ src/common/nodes/htmlNode.js
@@ -0,0 +1,33 @@
+import * as d3 from "d3";
+
+//静态变量
+
+
+
+//静态函数
+
+function HtmlNode(){
+    //实例属性
+    this.show=true;
+    this.group='';
+    this.shapes={
+
+    };
+    this.props={};
+}
+
+/*
+class HtmlNode{
+    constructor(){
+        this.show=true;
+        this.group='';
+        this.shapes={
+
+        };
+        this.props={};
+
+    }
+}
+ */
+
+export default HtmlNode
